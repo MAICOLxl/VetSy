7a10042d95cbf327229aa4d2013e41a327b7786b
Cesar Leonardo <bostonredyelow@hotmail.com>
2026-05-05 21:43:42 -0400

    implementacion de api en el front

---
+++ Js/login.js
@@ -0,0 +1,52 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+
+    const password = document.getElementById("password").value;
+
+    try {
+
+        const respuesta = await fetch("http://localhost:3000/auth/login", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                username,
+                password
+            })
+
+        });
+
+        const data = await respuesta.json();
+
+        if (respuesta.ok) {
+
+            localStorage.setItem(
+                "usuario",
+                JSON.stringify(data.user)
+            );
+
+            window.location.href = "index.html";
+
+        } else {
+
+            document.getElementById("mensaje").innerText =
+                data.message;
+
+        }
+
+    } catch (error) {
+
+        console.log(error);
+
+    }
+
+});
